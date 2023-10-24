35b3d611f0a3a009e22044f4f16439908fe925b6
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2023-10-24 13:35:35 +0100

    feat: Add react files

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@aprill.biz'
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: 'Antpnette',
+        email: 'shanna@melissa.tv'
+    },
+    {
+        id: 3,
+        name: 'Clenentine Bauch',
+        username: 'Sanantha',
+        email: 'Nathan@yesenia.net'
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: 'Karianne',
+        email: 'oconner@kory.org'
+    },
+    {
+        id: 5,
+        name: 'Chesley Dietrich',
+        username: 'Kanren',
+        email: 'mitchell@anon.com'
+    },
+    {
+        id: 6,
+        name: 'Mrs. Dennis Schulist',
+        username: 'Leopoldo_Corkey',
+        email: 'Karley_Dach@jasper.info'
+    },
+    {
+        id: 7,
+        name: 'Kurtis Weissnat',
+        username: 'Elwyn Skiles',
+        email: 'Kelly.Hogger@billy.biz'
+    },
+    {
+        id: 8,
+        name: 'Nicholas Runolfsdottir V',
+        username: 'Maxine_Nienow',
+        email: 'Sharwoo@rossamond.me'
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: 'Dolphine',
+        email: 'chris_mc@dana.io'
+    },
+    {
+        id: 10,
+        name: 'Clementina Dubuque',
+        username: 'Moriah.stanton',
+        email: 'Rey.padberg@karina.biz'
+    }
+]
